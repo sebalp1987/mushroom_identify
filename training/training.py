92e5e4bd6bb8d5400139e69993728cda89e1ef0a
Sebastián Mauricio Palacio <sebalp_1987@hotmail.com>
2019-09-17 16:08:30 +0200

    Add files via upload

--- training/training.py
+++ training/training.py
@@ -5,39 +5,41 @@ import cv2
 import numpy as np
 from sklearn.model_selection import train_test_split
 from keras.preprocessing.image import ImageDataGenerator
-from keras.preprocessing.image import img_to_array, load_img
 from keras.callbacks import EarlyStopping
 from keras import layers
 from keras import optimizers
 from keras import models
+from keras.utils import to_categorical
 
-
-train_angel = ['../data_input/train/{}'.format(i) for i in os.listdir(STRING.train) if 'angel' in i]
-train_kantarell = ['../data_input/train/{}'.format(i) for i in os.listdir(STRING.train) if 'kantarell' in i]
-
-train_imgs = train_angel + train_kantarell
-del train_angel, train_kantarell
-gc.collect()
+train_imgs = ['../data_input/train/{}'.format(i) for i in os.listdir(STRING.train)]
 
 # Resize
 nrows = 150
 ncolumns = 150
 channels = 3
 
-X = [] # images
-y = [] # labels
+X = []  # images
+y = []  # labels
+
+mushrooms = STRING.mushrooms
 
 for image in train_imgs:
     X.append(cv2.resize(cv2.imread(image, cv2.IMREAD_COLOR), (nrows, ncolumns), interpolation=cv2.INTER_CUBIC))
-    if 'angel' in image:
-        y.append(1)
-    elif 'kantarell' in image:
-        y.append(0)
+    value = -1
+    for key, val in mushrooms.items():
+        if key in image:
+            value = val
+    y.append(value)
 
 X = np.array(X)
 y = np.array(y)
 
+
 x_train, x_val, y_train, y_val = train_test_split(X, y, test_size=0.20, random_state=42)
+y_train = to_categorical(y_train)
+y_val = to_categorical(y_val)
+print(y_train)
+print(y_val)
 del X, y
 gc.collect()
 
@@ -45,20 +47,20 @@ ntrain = len(x_train)
 nval = len(x_val)
 
 model = models.Sequential()
-model.add(layers.Conv2D(32, (3,3), activation='relu', input_shape=(nrows, ncolumns, channels)))
+model.add(layers.Conv2D(32, (3, 3), activation='relu', input_shape=(nrows, ncolumns, channels)))
 model.add(layers.MaxPooling2D((2, 2)))
 model.add(layers.Conv2D(128, (3, 3), activation='relu'))
 model.add(layers.MaxPooling2D((2, 2)))
 model.add(layers.Conv2D(128, (3, 3), activation='relu'))
 model.add(layers.MaxPooling2D((2, 2)))
 model.add(layers.Flatten())
-model.add(layers.Dropout(0.5))
+model.add(layers.Dropout(0.2))
 model.add(layers.Dense(512, activation='relu'))
-model.add(layers.Dense(1, activation='sigmoid'))
+model.add(layers.Dense(len(mushrooms), activation='softmax'))
 
 model.summary()
 
-model.compile(loss='binary_crossentropy', optimizer=optimizers.RMSprop(lr=1e-4), metrics=['acc'])
+model.compile(loss='categorical_crossentropy', optimizer=optimizers.RMSprop(lr=1e-4), metrics=['acc'])
 
 # preprocess image
 train_datagen = ImageDataGenerator(rescale=1. / 255,  # Scale image to (0, 1)
