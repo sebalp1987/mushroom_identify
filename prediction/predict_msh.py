11c1d504d2a7f84b8414fef95f78b1a86ffafc86
Sebastián Mauricio Palacio <sebalp_1987@hotmail.com>
2019-09-17 16:08:14 +0200

    Add files via upload

--- prediction/predict_msh.py
+++ prediction/predict_msh.py
@@ -4,11 +4,15 @@ import cv2
 import numpy as np
 from keras.preprocessing.image import ImageDataGenerator
 import matplotlib.pyplot as plot
+import matplotlib.image as mpimg
 from keras.models import load_model
 
-test = ['../data_input/test/{}'.format(i) for i in os.listdir(STRING.test)]
-model = load_model(STRING.model_path + 'model.h5')
 
+test = ['../data_input/test/{}'.format(i) for i in os.listdir(STRING.test)]
+print(test)
+model = load_model(STRING.model_path + 'model.h5')
+mushrooms = STRING.mushrooms
+info_mush = STRING.explain_type
 X = [] # images
 nrows = 150
 ncolumns = 150
@@ -19,24 +23,31 @@ for image in test:
 X = np.array(X)
 datagen = ImageDataGenerator(rescale=1./255)
 
-columns = 5
-i = 0
 text_label = []
 pred_score = []
-plot.figure(figsize=(30, 20))
-for batch in datagen.flow(X, batch_size=1):
-    pred = model.predict(batch)
-    if pred > 0.5:
-        text_label.append('Destroying Angel')
-        pred_score.append(pred)
-    else:
-        text_label.append('Kantarell')
-        pred_score.append(1-pred)
-    plot.subplot(5/columns + 1, columns, i + 1)
-    plot.title('This is a ' + text_label[i] + ' with probability ' + str(pred_score[i]), fontsize=8)
-    imgplot = plot.imshow(batch[0])
-    i += 1
-    if i % 10 == 0:
-        break
 
-plot.show()
+
+j = 0
+for i, batch in enumerate(datagen.flow(X, batch_size=1)):
+    pred = model.predict(batch)
+    pred = [item for sublist in pred for item in sublist]
+    print(pred)
+    pred_score = max(pred)
+    ind_max = np.argmax(pred)
+    print(ind_max)
+    type_m = ""
+    info = ""
+    for key, val in mushrooms.items():
+        if val == ind_max:
+            type_m = key
+            info = info_mush.get(val)
+            break
+    print(info)
+
+    plot.title('This is a ' + type_m + ' with probability ' + str(pred_score), fontsize=8)
+    img = mpimg.imread(test[i])
+    plot.imshow(img)
+    plot.show()
+    j += 1
+    if j == len(test):
+        break
